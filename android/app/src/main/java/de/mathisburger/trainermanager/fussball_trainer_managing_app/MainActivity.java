adfaf94aa3f72b548319e8e37e01dc08d813aa43
MathisBurger <handymathis@gmail.com>
2020-05-21 21:54:19 +0200

    Initial commit

---
+++ android/app/src/main/java/de/mathisburger/trainermanager/fussball_trainer_managing_app/MainActivity.java
@@ -0,0 +1,6 @@
+package de.mathisburger.trainermanager.fussball_trainer_managing_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
